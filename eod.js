9a34ea54ba5046291262d5ab10f8caa480c3445b
Aleksei.Tirman <aleksei.tirman@jetbrains.com>
2025-10-01 10:25:00 +0300

    Render treasures and pings

--- eod.js
+++ eod.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const delColor = "rgb(34, 51, 55)";
 const title = "ECHOES OF THE DEEP";
 
@@ -16,7 +18,16 @@ for (let r = 0; r < fieldMaxRows; r++) {
     }
 }
 
+field[5][2] = Treasure;
+field[0][0] = Treasure;
 field[4][4] = Treasure;
+let numTreasures = 3; // TODO: Compute
+
+let pingsUsed = 0;
+
+// TODO: Loading or menu
+let treasureImage = new Image();
+treasureImage.src = "assets/sprites/treasure_64x64.png";
 
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
@@ -35,25 +46,38 @@ for (let i = 0; i < 10; i++) {
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let clickedCell = {row: -1, col: -1};
 let pad = 32;
 let fieldPos = {x: pad, y: pad + titleHeight + 20};
 
 let fieldWidth = canvas.width - pad * 2;
 let cellSize = fieldWidth / fieldMaxCols;
-let actualWidth = cellSize * fieldMaxCols;
-let actualHeight = cellSize * fieldMaxRows;
+let fieldActualWidth = cellSize * fieldMaxCols;
+let fieldActualHeight = cellSize * fieldMaxRows;
 
 window.addEventListener("click", function (e) {
     let col = Math.floor((e.clientX - fieldPos.x) / cellSize);
     let row = Math.floor((e.clientY - fieldPos.y) / cellSize);
 
-    if (col >= 0 && row >= 0 && col < fieldMaxCols && row < fieldMaxRows) {
-        clickedCell.row = row;
-        clickedCell.col = col;
-    }
+    if (col >= 0 && row >= 0 && col < fieldMaxCols && row < fieldMaxRows && numTreasures > 0) {
+        let content = field[row][col];
+        if (content === Nothing) {
+            let minDistance = Math.max(fieldMaxRows, fieldMaxCols);
+            for (let r = 0; r < fieldMaxRows; r++) {
+                for (let c = 0; c < fieldMaxCols; c++) {
+                    if (field[r][c] === Treasure) {
+                        let dist = distance({row, col}, {row: r, col: c});
+                        if (dist < minDistance) minDistance = dist;
+                    }
+                }
+            }
 
-    console.log(clickedCell);
+            pingsUsed++;
+            field[row][col] = minDistance;
+        } else if (content === Treasure) {
+            field[row][col] = RevealedTreasure;
+            numTreasures--;
+        }
+    }
 });
 
 function distance(cell1, cell2) {
@@ -82,45 +106,72 @@ requestAnimationFrame(function update() {
 
     for (let r = 0; r < fieldMaxRows + 1; r++) {
         ctx.moveTo(0, r * cellSize);
-        ctx.lineTo(actualWidth, r * cellSize);
+        ctx.lineTo(fieldActualWidth, r * cellSize);
     }
 
     for (let c = 0; c < fieldMaxCols + 1; c++) {
         ctx.moveTo(c * cellSize, 0);
-        ctx.lineTo(c * cellSize, actualHeight);
+        ctx.lineTo(c * cellSize, fieldActualHeight);
     }
 
     ctx.stroke();
 
-    if (clickedCell.row >= 0 && clickedCell.col >= 0) {
-        ctx.font = "32px Share Tech Mono, monospace";
-
-        let minDistance = Math.max(fieldMaxRows, fieldMaxCols);
-        for (let r = 0; r < fieldMaxRows; r++) {
-            for (let c = 0; c < fieldMaxCols; c++) {
-                if (field[r][c] === Treasure) {
-                    let dist = distance(clickedCell, {row: r, col: c});
-                    if (dist < minDistance) minDistance = dist;
-                }
-            }
-        }
-
-        let numDim = numDims[minDistance];
-        let x = clickedCell.col  * cellSize + cellSize/2 - numDim.width/2;
-        let y = clickedCell.row * cellSize + numDim.actualBoundingBoxAscent + cellSize/2 - (numDim.actualBoundingBoxDescent - numDim.actualBoundingBoxAscent)/2;
-        ctx.fillText(minDistance.toString(10), x, y);
-    }
-
     for (let r = 0; r < fieldMaxRows; r++) {
         for (let c = 0; c < fieldMaxCols; c++) {
-            if (field[r][c] === Treasure) {
-                ctx.save();
-                ctx.fillStyle = "red";
-                ctx.fillRect(c * cellSize, r  * cellSize, cellSize, cellSize);
-                ctx.restore();
+            let content = field[r][c];
+
+            switch (content) {
+                case Nothing: {
+                    // Render nothing
+                } break;
+                case Treasure: {
+                    // TODO: Hide after
+                    ctx.save();
+                    ctx.fillStyle = "red";
+                    ctx.fillRect(c * cellSize + 10, r  * cellSize + 10, 4, 4);
+                    ctx.restore();
+                } break;
+                case RevealedTreasure: {
+                    let w = cellSize/1.5;
+                    let h = cellSize/1.5;
+                    let x = c  * cellSize + cellSize/2 - w / 2;
+                    let y = r * cellSize + cellSize/2 - w / 2;
+
+                    ctx.drawImage(treasureImage, x, y, w, h);
+                } break;
+                default: {
+                    let dist = content;
+                    let numDim = numDims[dist];
+                    let x = c  * cellSize + cellSize/2 - numDim.width/2;
+                    let y = r * cellSize + numDim.actualBoundingBoxAscent + cellSize/2 - (numDim.actualBoundingBoxDescent - numDim.actualBoundingBoxAscent)/2;
+                    ctx.font = "32px Share Tech Mono, monospace";
+                    ctx.fillText(dist.toString(10), x, y);
+                } break;
             }
         }
     }
+
+    ctx.translate(0, fieldActualHeight);
+    ctx.translate(0, 24);
+
+    const gradient = ctx.createLinearGradient(0, 0, fieldActualWidth, 0);
+
+    gradient.addColorStop(0, "#242a2e");
+    gradient.addColorStop(0.5, "#2c717d");
+    gradient.addColorStop(1, "#24272c");
+
+    ctx.save();
+    ctx.fillStyle = gradient;
+    ctx.fillRect(0, 0, fieldActualWidth, 2);
+    ctx.restore();
+
+    ctx.translate(0, 16);
+
+    ctx.save();
+    ctx.font = "16px Share Tech Mono, monospace";
+    ctx.fillStyle = "#c9c7c7";
+    ctx.fillText(`PINGS USED: ${pingsUsed}`, 0, 0);
+    ctx.restore();
 
     ctx.restore();
 
